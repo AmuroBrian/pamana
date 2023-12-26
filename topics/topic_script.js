00086bfeee1899669ceb2636b89d3c132d17cf31
AmuroBrian <perezbrian.sti@gmail.com>
2023-12-26 08:34:08 +0800

    Adding content pages

--- topics/topic_script.js
+++ topics/topic_script.js
@@ -14,7 +14,10 @@ const description = document.querySelectorAll(".description");
 const observerLeft = new IntersectionObserver((entries) =>{
     entries.forEach(entry => {
         entry.target.classList.toggle("showLeft", entry.isIntersecting);
-    })
+    }),
+    {
+        threshold: 1,
+    }
 })
 
 observerLeft.observe(slides[0]);
@@ -26,7 +29,10 @@ observerLeft.observe(description[3]);
 const observerRight = new IntersectionObserver((entries) =>{
     entries.forEach(entry => {
         entry.target.classList.toggle("showRight", entry.isIntersecting);
-    })
+    }),
+    {
+        threshold: 1,
+    }
 })
 
 observerRight.observe(slides[1]);
@@ -34,3 +40,29 @@ observerRight.observe(slides[3]);
 observerRight.observe(description[0]);
 observerRight.observe(description[2]);
 observerRight.observe(description[4]);
+
+const precolonial = document.querySelector('#precolonial');
+const spanish = document.querySelector('#spanish');
+const american = document.querySelector('#american');
+const japanese = document.querySelector('#japanese');
+const modern = document.querySelector('#modern');
+
+precolonial.addEventListener('click',() => {
+    window.location.href = "../precolonial/precolonial.html";
+})
+
+spanish.addEventListener('click', ()=>{
+    window.location.href = "../spanish/spanish.html";
+})
+
+american.addEventListener('click', ()=>{
+    window.location.href = "../american/american.html";
+})
+
+japanese.addEventListener('click', ()=>{
+    window.location.href = "../japanese/japanese.html";
+})
+
+modern.addEventListener('click', ()=>{
+    window.location.href = "../modern/modern.html";
+})
